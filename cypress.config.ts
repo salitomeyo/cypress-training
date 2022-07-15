92e026d8632858217000bf8ddbe043257a65de76
Salome Aristizabal Giraldo <53347835+salitomeyo@users.noreply.github.com>
2022-07-15 08:27:46 -0500

    feat: buying shirt automation practice (#7)
    * feat: buying shirt automation practice
    
    * fix: eslint doublequote

--- cypress.config.ts
+++ cypress.config.ts
@@ -7,9 +7,9 @@ export default defineConfig({
     video: false,
     screenshotOnRunFailure: false,
     setupNodeEvents(on, config) {
-      // modify config values examples
-      // config.defaultCommandTimeout = 10000
-
+      config.defaultCommandTimeout = 20000
+      config.responseTimeout = 20000
+    
       // IMPORTANT return the updated config object
       return config
     }
