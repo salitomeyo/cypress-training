16843345f2dfa27c1f79c24b0988d66c18384e3d
Salome Aristizabal Giraldo <salitomeyo16@gmail.com>
2022-07-25 16:23:35 -0500

    feat: without retries

--- cypress/e2e/iframe.cy.ts
+++ cypress/e2e/iframe.cy.ts
@@ -2,22 +2,19 @@ import {
   IframePage,
 } from "../page/index";
 
-describe("Working with an iframe", {
-  retries: {
-    runMode: 3,
-    openMode: 2,
-  },
-}, () => {
+describe("Working with an iframe", () => {
   let iframePage: IframePage;
+  let iframeTitle: string;
 
   before(() => {
     iframePage = new IframePage();
+    iframeTitle = "HTML Tutorial";
 
     iframePage.visit();
   });
 
-  it("then an iframe should be selected", () => {
-    iframePage.getFrameTitle().should("eq", "HTML Tutorial");
+  it("then an iframe should be interactive", () => {
+    iframePage.getFrameTitle().should("eq", iframeTitle);
 
     iframePage.goToCssPageInFrame();
   });
